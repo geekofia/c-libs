2aefcc56e2245fc38d033c2ecbc158e9548a92a6
chankruze <chankruze@gmail.com>
2019-09-26 05:25:49 +0530

    Add new functions & a standard header
    * Add standard header file to include functions for now
    * Add few comments, use camel casing in function names
    
    Signed-off-by: chankruze <chankruze@gmail.com>

--- custom_functions.h
+++ custom_functions.h
@@ -2,19 +2,57 @@
  * @Author: chankruze (Chandan Kumar Mandal) 
  * @Date: 2019-06-23 11:59:14 
  * @Last Modified by: chankruze (Chandan Kumar Mandal)
- * @Last Modified time: 2019-06-23 12:10:31
+ * @Last Modified time: 2019-09-26 03:10:30
  */
+/*===============*/
+/* Preprocessors */
+/*===============*/
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h> // For ioctl, TIOCGWINSZ
+#include <unistd.h> // For STDOUT_FILENO
+
+/*===========*/
+/* Functions */
+/*===========*/
+
+// FN-01
 /**
- * name: showbits
+ * This function show the bits of a given integer
+ * 
+ * name: showBits
  * params: int
  * return: void 
 */
-void showbits(int n)
-{
-    for (int i = 8; i > 0; i--)
-    {
+void showBits(int n){
+    for (int i = 8; i > 0; i--){
         n & (1 << (i - 1)) ? printf("1") : printf("0");
     }
+}
+
+// FN-02
+/**
+ * This simple function clears the output screen. Just like clrscr()
+ * 
+ * name: clearScr
+ * params: none
+ * return: void 
+*/
+void clearScr(){
+    printf("\033[H\033[J");
+}
+
+// FN-03
+/**
+ * Function to get terminal size (rows & colums).
+ * 
+ * This function uses ioctl with the standard output file number STDOUT_FILENO and TIOCGWINSZ.
+ * It sets output terminal info to a winsize structure which address is provided as param.
+ * 
+ * name: getTerminal
+ * params: struct winsize *ptr
+ * return: void
+*/
+void getTerminal(struct winsize *terminal){
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, terminal);
 }
---
+++ stdgeekofia.h
@@ -0,0 +1,6 @@
+#define CUSTOM_FNS "custom_functions.h"
+#define SMILEY "\u263A "
+
+#include <stdio.h>
+
+#include CUSTOM_FNS
